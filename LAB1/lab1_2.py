db85d1b74b44b9df28effd50c9e5c29593d54a6c
Ahmet Arslan <101971348+ahmetarslanS@users.noreply.github.com>
2023-03-14 17:34:16 +0300

    Uploading Lab1 again as a file.

---
+++ LAB1/lab1_2.py
@@ -0,0 +1,10 @@
+
+var1 = int(input("Enter the first value.\n"))
+var2 = int(input("Enter the second value.\n"))
+
+sum = var1 + var2
+diff = var1 - var2
+prod = var1 * var2
+
+print("First value is :", var1, "\nSecond value is :", var2, "\nSummation:", sum, "Difference:", diff,
+      "Multiplication:", prod)
---
+++ LAB1/myFirstLabScript.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name?\n")
+
+print("Hello " + name)
+
+id = input("What is your Student ID?\n")
+
+print("Your ID is " + id)
